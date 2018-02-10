1a1bbe1c9864e207a9fd01a92f37d3da0ad3dd29
funnytseric <31648496+funnytseric@users.noreply.github.com>
2018-02-10 14:51:02 +0800

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,12 +12,53 @@ namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
+        Random rand = new Random();
         public Form1()
         {
             InitializeComponent();
 
+            int[] a = new int[10];
+            string str = "numbers before sorted: ";
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = rand.Next(100);
+                str += a[i].ToString() + " ";
             }
-            int debug = 0;
+            str += "\n";
+
+            Sort(a, 0, a.Length-1);
+            for (int i = 0; i < a.Length; i++)
+                str += a[i].ToString() + " ";
+
+            MessageBox.Show(str);
+        }
+
+        private void Swap(int[] ary, int a, int b)
+        {
+            int temp = ary[a];
+            ary[a] = ary[b];
+            ary[b] = temp;
+        }
+
+        private void Sort(int[] ary, int left, int right)
+        {
+            if (left >= right)
+                return;
+
+            int pivot = left;
+            int pivotVal = ary[pivot];
+            int swapIndex = left;
+
+            Swap(ary, pivot, right);
+            for(int i=left; i<right;i++)
+            {
+                if (ary[i] <= pivotVal)
+                    Swap(ary, i, swapIndex++);
+            }
+            Swap(ary, swapIndex, right);
+
+            Sort(ary, left, swapIndex - 1);
+            Sort(ary, swapIndex + 1, right);
         }
     }
 }
