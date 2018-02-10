e4d42523c8c63de31c0ebce3834acf8472743db2
funnytseric <31648496+funnytseric@users.noreply.github.com>
2018-02-10 16:12:43 +0800

    Add function for "Island Perimeter"
    add a function for solving the "Island Perimeter" problem on LeetCode.
    
    LeetCode Algorithm 463. Island Perimeter
    https://leetcode.com/problems/island-perimeter/description/

--- Form1.cs
+++ Form1.cs
@@ -60,5 +60,46 @@ namespace WindowsFormsApplication1
             Sort(ary, left, swapIndex - 1);
             Sort(ary, swapIndex + 1, right);
         }
+
+        public int IslandPerimeter(int[,] grid)
+        {
+            int perimeter = 0;
+            int height = grid.GetLength(0);
+            int width = grid.GetLength(1);
+
+            //original code, not os effecient
+            /*for(int h=0; h<height; h++)
+                for(int w=0; w<width; w++)
+                {
+                    if(grid[h,w] == 1)
+                    {
+                        perimete += 4;
+                        if(h-1 >= 0)
+                            perimete -= grid[h-1,w];
+                        if(h+1 < height)
+                            perimete -= grid[h+1,w];
+                        if(w-1 >= 0)
+                            perimete -= grid[h,w-1];
+                        if(w+1 < width)
+                            perimete -= grid[h,w+1];
+                    }
+                }*/
+
+            for (int h = 0; h < height; h++)
+                for (int w = 0; w < width; w++)
+                {
+                    if (grid[h, w] == 1)
+                    {
+                        perimeter += 4;
+
+                        if (h > 0 && grid[h - 1, w] == 1)
+                            perimeter -= 2;
+                        if (w > 0 && grid[h, w - 1] == 1)
+                            perimeter -= 2;
+                    }
+                }
+
+            return perimeter;
+        }
     }
 }
