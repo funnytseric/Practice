d56581f37ac2f3a4bf602812b0775a99976cc6c7
funnytseric <31648496+funnytseric@users.noreply.github.com>
2018-02-10 13:37:23 +0800

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -16,37 +16,8 @@ namespace WindowsFormsApplication1
         {
             InitializeComponent();
 
-            Stack<>
             }
             int debug = 0;
-        }
-
-        private bool test()
-        {
-            int[,] matrix = new int[,] { { 1, 2, 3, 4 }, { 5, 1, 2, 3 }, { 9, 5, 1, 2 } };
-            int num;
-            int row = matrix.GetLength(0);
-            int col = matrix.GetLength(1);
-
-            if (row <= 1 || col <= 1)
-                return false;
-            else
-            {
-                for (int i = 0; i < row - 1; i++)
-                    for (int j = 0; j < col - 1; j++)
-                    {
-                        num = matrix[i, j];
-                        int k = 0;
-                        while (k + i < row && k+j < col)
-                        {
-                            if (matrix[i + k, j + k] != num)
-                                return false;
-                            else
-                                k++;
-                        }
-                    }
-            }
-            return true;
         }
     }
 }
